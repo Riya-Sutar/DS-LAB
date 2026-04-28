a79b6e7337a111b31234b50f469f9d291ac208b7
Riya <riya.sutar07@gmail.com>
2026-04-28 11:03:28 +0530

    Add files via upload

---
+++ BST_TRAVERSAL.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left, *right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+Node* findMin(Node* node) {
+    while (node && node->left != nullptr)
+        node = node->left;
+    return node;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == nullptr) return root;
+
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+    else {
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root) {
+    if (!root) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (!root) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = new Node(50);
+    root->left = new Node(30);
+    root->right = new Node(70);
+    root->left->left = new Node(20);
+    root->left->right = new Node(40);
+
+    cout << "Inorder: ";
+    inorder(root);
+    cout << endl;
+
+    root = deleteNode(root, 30);
+
+    cout << "After deletion: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
